68dbbcd29cad3d97ac8e7548c4082ad943cc1fdf
Aleksandr <al.novikov08@gmail.com>
2022-07-29 14:42:22 +0300

    add AddTwoNumbers problem

---
+++ medium/add-two-numbers/add_two_numbers.go
@@ -0,0 +1,81 @@
+package add_two_numbers
+
+/*
+runtime: 22 ms
+memory: 4.7 MB
+*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func (n *ListNode) AddNode(value int) {
+	tail := &n
+
+	for {
+		if (*tail).Next == nil {
+			break
+		}
+		tail = &(*tail).Next
+	}
+
+	newNode := ListNode{Val: value}
+	(*tail).Next = &newNode
+}
+
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var rootSum, prevNode *ListNode
+
+	currentL1, currentL2 := l1, l2
+	val1, val2 := currentL1.Val, currentL2.Val
+
+	carry := 0
+	setCarry := false
+
+	validIteration := true
+	for validIteration {
+		sum := val1 + val2 + carry
+
+		if setCarry {
+			carry = 0
+			setCarry = false
+		}
+
+		if sum > 9 {
+			carry = sum / 10
+			setCarry = true
+			sum %= 10
+		}
+
+		newNode := ListNode{Val: sum}
+		if rootSum == nil {
+			rootSum = &newNode
+		} else {
+			prevNode.Next = &newNode
+		}
+		prevNode = &newNode
+
+		validIteration = false
+		if currentL1.Next == nil {
+			val1 = 0
+		} else {
+			currentL1 = currentL1.Next
+			val1 = currentL1.Val
+			validIteration = true
+		}
+
+		if currentL2.Next == nil {
+			val2 = 0
+		} else {
+			currentL2 = currentL2.Next
+			val2 = currentL2.Val
+			validIteration = true
+		}
+	}
+
+	if carry > 0 {
+		prevNode.Next = &ListNode{Val: carry}
+	}
+
+	return rootSum
+}
---
+++ medium/add-two-numbers/add_two_numbers_test.go
@@ -0,0 +1,62 @@
+package add_two_numbers
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+
+	testCases := []struct {
+		root1       []int
+		root2       []int
+		expectedSum []int
+	}{
+		{root1: []int{2, 4, 3},
+			root2: []int{5, 6, 4}, expectedSum: []int{7, 0, 8}},
+		{root1: []int{0, 4, 3},
+			root2: []int{0, 6, 4}, expectedSum: []int{0, 0, 8}},
+		{root1: []int{1, 0, 0, 0, 0, 1},
+			root2: []int{5, 6, 4}, expectedSum: []int{6, 6, 4, 0, 0, 1}},
+		{root1: []int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			root2: []int{5, 6, 4}, expectedSum: []int{6, 6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}},
+	}
+
+	for _, input := range testCases {
+		input := input
+
+		t.Run(fmt.Sprintf("%v + %v = %v", input.root1, input.root2, input.expectedSum), func(t *testing.T) {
+			root1 := getLinkedList(input.root1...)
+			root2 := getLinkedList(input.root2...)
+			actualSum := AddTwoNumbers(root1, root2)
+
+			require.Equal(t, input.expectedSum, unwrapLinkedList(actualSum))
+		})
+	}
+}
+
+func getLinkedList(numbers ...int) *ListNode {
+	root := ListNode{Val: numbers[0] % 10}
+	numbers = numbers[1:]
+
+	for _, v := range numbers {
+		root.AddNode(v)
+	}
+
+	return &root
+}
+
+func unwrapLinkedList(node *ListNode) []int {
+	result := make([]int, 0)
+
+	current := node
+	for {
+		if current == nil {
+			break
+		}
+		result = append(result, current.Val)
+		current = current.Next
+	}
+	return result
+}
