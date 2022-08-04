0eeb177c966e084c552c0cfd95eb9d4a30cc27a3
Aleksandr <al.novikov08@gmail.com>
2022-08-04 10:50:14 +0300

    add two pointers-pattern example (not working for current problem, because array is not sorted)

--- easy/two_sum/two_sum.go
+++ easy/two_sum/two_sum.go
@@ -1,26 +1,6 @@
 package two_sum
 
 /*
-runtime: 63 ms
-memory: 3.5 MB
-*/
-func TwoSumSlow(nums []int, target int) []int {
-
-	for i, num := range nums {
-		for j, n := range nums {
-			if i == j {
-				continue
-			}
-			if (num + n) == target {
-				return []int{i, j}
-			}
-		}
-	}
-
-	return nil
-}
-
-/*
 runtime: 9 ms
 memory: 5.6 MB
 */
@@ -46,3 +26,47 @@ func TwoSum(nums []int, target int) []int {
 
 	return nil
 }
+
+/*
+runtime: 63 ms
+memory: 3.5 MB
+*/
+func TwoSumSlow(nums []int, target int) []int {
+
+	for i, num := range nums {
+		for j, n := range nums {
+			if i == j {
+				continue
+			}
+			if (num + n) == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return nil
+}
+
+/*
+WORKS ONLY FOR SORTED ARRAY(!)
+
+pattern: two pointers
+func TwoSum(nums []int, target int) []int {
+	start, end := 0, len(nums)-1 // 0 2
+	for start < end {
+		sum := nums[start] + nums[end] //6
+		if sum == target {
+			return []int{start, end}
+		}
+		if sum < target {
+			start++
+		} else {
+			end--
+		}
+	}
+
+	return []int{start, end}
+}
+
+WORKS ONLY FOR SORTED ARRAY(!)
+*/
